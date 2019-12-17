a29f28b012d7f3764488a2c7f6e5ce3dc8c2b01a
raysechai <raysechai1124@gmail.com>
2019-12-17 23:29:10 +0800

    Update bookmark_screen.dart

--- bookmark_screen.dart
+++ bookmark_screen.dart
@@ -5,11 +5,54 @@ class BookmarkScreen extends StatefulWidget {
   _BookmarkScreenState createState() => _BookmarkScreenState();
 }
 
-class _BookmarkScreenState extends State<BookmarkScreen> {
+class _BookmarkScreenState extends State<BookmarkScreen> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bookmark'),
+        bottom: TabBar(
+          tabs: [
+            Tab(text: 'Location'),
+            Tab(text: 'Itinerary'),
+          ],
+          controller: _tabController,
+          indicatorColor: Colors.white,
+        ),
+      ),
+      body: TabBarView(
+        children: [
+          BookmarkLocation(),
+          BookmarkItinerary(),
+        ],
+        controller: _tabController,
+      ),
+    );
+  }
+}
+
+class BookmarkLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Location'),
+    );
+  }
+}
+
+class BookmarkItinerary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Itinerary'),
     );
   }
 }
