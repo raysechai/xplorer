b11bb4f30fac23d70669246f691ba44c37068a97
raysechai <raysechai1124@gmail.com>
2019-12-19 11:25:06 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,41 +1,46 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:testing2/screens/home_screen.dart';
-import 'package:testing2/screens/login_screen.dart';
-import 'package:testing2/screens/signup_screen.dart';
+import 'package:testing2/services/auth.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget _getScreenId() {
-    return StreamBuilder<FirebaseUser>(
-      stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (BuildContext context, snapshot) {
-        if (snapshot.hasData) {
-          return HomeScreen();
-        } else {
-          return LoginScreen();
-        }
-      }
-    );
-  }
+  // Widget _getScreenId() {
+  //   return StreamBuilder<FirebaseUser>(
+  //     stream: FirebaseAuth.instance.onAuthStateChanged,
+  //     builder: (BuildContext context, snapshot) {
+  //       if(snapshot.hasData) {
+  //         return HomeScreen();
+  //       } else {
+  //         return LoginScreen();
+  //       }
+  //     },
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Xplorer', 
-      debugShowCheckedModeBanner: false,
-      home: _getScreenId(),
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        accentColor: Colors.black,
-        scaffoldBackgroundColor: Colors.white,
-        canvasColor: Colors.transparent,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'Xplorer',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            accentColor: Colors.blueAccent,
+            scaffoldBackgroundColor: Colors.white,
+            canvasColor: Colors.transparent,
+          ),
+          //home: auth.isAuth ? HomeScreen() : LoginScreen(),
+          home: HomeScreen(),
+        ),
       ),
-      routes: {
-        LoginScreen.id: (context) => LoginScreen(),
-        SignupScreen.id: (context) => SignupScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
-      }
     );
   }
 }
